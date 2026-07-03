8ec571c3829ad496d59b6335009f9a5bd883c8f5
YuriyStepanishin <y.stepanishin@gmail.com>
2026-07-03 11:39:45 +0300

    Remove duplicated project folder

--- privat_project/SODA/soda-beckend/src/constants/roles.js
+++
@@ -1,7 +0,0 @@
-export const ROLES = {
-  ADMIN: 'admin',
-  MANAGER: 'manager',
-  SUPERVISOR: 'supervisor',
-  AGENT: 'agent',
-  BRAND_MANAGER: 'brand_manager',
-};
--- privat_project/SODA/soda-beckend/src/validators/salesSchemas.js
+++
@@ -1,7 +0,0 @@
-import Joi from 'joi';
-
-export const salesPeriodSchema = Joi.object({
-  dateFrom: Joi.date().required(),
-
-  dateTo: Joi.date().required(),
-});
